df37d4611b9b3b6b78d08fc5d585ccbfa19abfe9
Vinit Mehta <vinit2004mehtaa@gmail.com>
2023-11-22 03:05:09 +0530

    readme and client changes

--- client/delete.c
+++ client/delete.c
@@ -19,7 +19,7 @@ void delete_operation(char *path,int macro)
     }
     else
     {
-        printf(RED("Deletion of Directory or File not succesfull : %s\n"),response->request_type); // Error Not succesfull
+        printf(RED("Deletion of Directory or File not succesfull : %s\n"), response->data); // Error Not succesfull
     }
     close(client_socket);
 }
